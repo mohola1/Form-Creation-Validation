9b1f02841f7115150c81bb13d10b2cfda94681cf
Mohammed <mohammedolamilekan1@gmail.com>
2025-10-08 19:53:15 +0100

    Update

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 document.addEventListener("DOMContentLoaded", function () {
     //Get the form
-    const form = document.getElementById(registration-form)
+    const form = document.getElementById(registration-form);
 
     //Get the division where feedback will be displayed
-    const feedbackDiv = document.getElementById(form-feedback)
+    const feedbackDiv = document.getElementById(form-feedback);
+
+    //Add a submit event listener
+    form.addEventListener("submit", function (event){
+        const username = document.getElementById("username").value.trim();
+        const email = document.getElementById("email").value.trim();
+        const password = document.getElementById("password").value.trim();
+    })
 }
 )
