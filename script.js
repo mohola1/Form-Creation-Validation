c1b6f0bb56426c3e8c781cba5395523732d618bf
Mohammed <mohammedolamilekan1@gmail.com>
2025-10-08 07:59:52 +0100

    Update

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 document.addEventListener("DOMContentLoaded", function () {
     //Get the form
+    const form = document.getElementById(registration-form)
+
+    //Get the division where feedback will be displayed
+    const feedbackDiv = document.getElementById(form-feedback)
 }
 )
