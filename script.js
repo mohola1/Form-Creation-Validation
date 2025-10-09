dbc075337b658c87440026bbff5b59cfa13f1019
Mohammed <mohammedolamilekan1@gmail.com>
2025-10-09 09:24:33 +0100

    Update

--- script.js
+++ script.js
@@ -7,9 +7,27 @@ document.addEventListener("DOMContentLoaded", function () {
 
     //Add a submit event listener
     form.addEventListener("submit", function (event){
+        // Initialize validation variables
+        let isValid = true;
+        let messages = [];
+        // Username validation
         const username = document.getElementById("username").value.trim();
+        if (username.length < 3) {
+            isValid = false;
+            messages.push("Username must be at least 3 characters long.");
+        }
+        // Email validation
         const email = document.getElementById("email").value.trim();
+        if (!email.includes("@") || !email.includes(".")){
+            isValid = false;
+            messages.push("Please enter a valid email address.");
+        }
+        // Password validation
         const password = document.getElementById("password").value.trim();
+        if (password.length < 8) {
+            isValid = false;
+            messages.push("Password must be at least 8 character long");
+        }
     })
 }
 )
